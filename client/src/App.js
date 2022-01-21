2e4bd946b285356514e50a89df38b2ea374e29e2
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2022-01-21 13:36:16 +0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Buttons</Button>
+    </div>
+  );
+}
+
+export default App;
