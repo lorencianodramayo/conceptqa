eaee76e3059e89076c40917eea980987b7ff80b1
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2022-02-02 12:28:08 +0800

    updated code to check if Adlib variable is defined in creative

--- server/routes/TemplateAPI.js
+++ server/routes/TemplateAPI.js
@@ -97,28 +97,30 @@ router.post("/upload", (req, res) => {
                     }, '*');
                   }, 500);
 
-                  Adlib.localTimeline = function(status){
-                    status = status||null;
-                    switch(status){
-                        case "PLAY":
-                          if(_obj.timelineEvent == null){
-                              var t = "0.0";
-                              _obj.timelineEvent = setInterval(function(){
-                                  t = (gsap.globalTimeline.time().toFixed(1)> 0.5)? gsap.globalTimeline.time().toFixed(1) : t ;
-                                  parent.postMessage({
-                                    type: 'CREATIVE_TIME', t
-                                  }, '*');
-                              }, 100);
-                          }
-                        break;
-                        case "PAUSE":
-                            clearInterval(_obj.timelineEvent);
-                            _obj.timelineEvent=null;
-                        break;
-                        case "END":
-                            clearInterval(_obj.timelineEvent);
-                            _obj.timelineEvent=null;
-                        break;
+                  if(typeof Adlib!=='undefined'){
+                    Adlib.localTimeline = function(status){
+                      status = status||null;
+                      switch(status){
+                          case "PLAY":
+                            if(_obj.timelineEvent == null){
+                                var t = "0.0";
+                                _obj.timelineEvent = setInterval(function(){
+                                    t = (gsap.globalTimeline.time().toFixed(1)> 0.5)? gsap.globalTimeline.time().toFixed(1) : t ;
+                                    parent.postMessage({
+                                      type: 'CREATIVE_TIME', t
+                                    }, '*');
+                                }, 100);
+                            }
+                          break;
+                          case "PAUSE":
+                              clearInterval(_obj.timelineEvent);
+                              _obj.timelineEvent=null;
+                          break;
+                          case "END":
+                              clearInterval(_obj.timelineEvent);
+                              _obj.timelineEvent=null;
+                          break;
+                      }
                     }
                   }
                   
