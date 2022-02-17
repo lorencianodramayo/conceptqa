554a57f7bcc879282d9a34fbeee01d9acd8f2947
Lorenciano Dramayo <lorencianodramayo@gmail.com>
2022-02-18 02:25:25 +0800

    added extra

--- server/routes/CreativesAPI.js
+++ server/routes/CreativesAPI.js
@@ -4,12 +4,23 @@ const router = express.Router();
 const CreativesModel = require("../models/CreativesModel");
 
 router.post("/", (req, res) => {
-
     CreativesModel.deleteMany({}, (err, data) => {
-        console.log(data);
-    })
-    console.log(req);
-    return res.send({msg: "hello"})
+        if (err) {
+          return res.status(500).json({ msg: "Sorry, internal server errors" });
+        }
+
+        const creatives = new CreativesModel(req.body.data[0]);
+
+        creatives.save((error, result) => {
+          if (error) {
+            return res
+              .status(500)
+              .json({ msg: "Sorry, internal server errors" });
+          }
+
+          return res.json(result);
+        });
+    });
 });
 
 module.exports = router;
